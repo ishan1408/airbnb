2fbe89a7c4de98d87e6ebf530af767b95f4303dd
ishan1408 <ishan@startupflora.com>
2025-07-31 12:57:00 +0530

    updated api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://airbnb-backend-it28.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) =>
